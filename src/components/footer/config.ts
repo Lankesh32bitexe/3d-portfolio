97227673a4d16f8d63330ae02020192767af8ee5
Your Name <you@example.com>
2026-04-09 14:23:43 +0530

    Initial commit: Rebranded portfolio for Asutosh Rout

---
+++ src/components/footer/config.ts
@@ -0,0 +1,12 @@
+const footer: { title: string; href: string }[] = [
+  {
+    title: "Blog",
+    href: "https://asutosh-rout.hashnode.dev/",
+  },
+  {
+    title: "Newsletter",
+    href: "https://asutosh-rout.hashnode.dev/",
+  },
+];
+
+export { footer };
---
+++ src/data/config.ts
@@ -0,0 +1,43 @@
+const config = {
+  title: "Asutosh Rout | AI Enthusiast",
+  description: {
+    long: "Explore the portfolio of Asutosh, a Computer Science Engineer with expertise in Development, DevOps & Database management. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
+    short:
+      "Discover the portfolio of Asutosh, an AI enthusiast solving real life problems in DevOps and Development.",
+  },
+  keywords: [
+    "Asutosh",
+    "Asutosh Rout",
+    "asutoshrout",
+    "asutoshrout.com",
+    "Asutosh Rout portfolio",
+    "portfolio",
+    "DevOps",
+    "Development",
+    "Database",
+    "AI Docker file optimizer",
+    "Finance me",
+    "creative technologist",
+    "web development",
+    "interactive websites",
+    "AWS",
+    "DevSecOps",
+    "AIOps",
+    "Azure",
+  ],
+  author: "Asutosh Rout",
+  email: "asutoshrout@gmail.com",
+  site: "https://asutoshrout.com",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/asutosh_rout/",
+    linkedin: "https://www.linkedin.com/in/asutosh-rout/",
+    instagram: "https://www.instagram.com/asutosh_rout/",
+    facebook: "https://www.facebook.com/asutosh.rout",
+    github: "https://github.com/AsutoshRout/",
+  },
+};
+export { config };
